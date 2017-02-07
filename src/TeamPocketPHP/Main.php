1a84db7cf2a80f4f059eddaafda5e357dc12cbc4
orlando.comas62 <orlando.comas62@gmail.com>
2017-02-06 19:57:49 -0500

    1

--- src/TeamPocketPHP/Main.php
+++ src/TeamPocketPHP/Main.php
@@ -3,16 +3,24 @@
 namespace TeamPocketPHP;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\command\PluginCommand;
 
-class Main extends PluginBase {
+use TeamPocketPHP\game\game;
+
+class Main extends PluginBase{
 	
-		
-	public function onEnable() {
-		
-		  $this->getLogger()->info("UHC Simulator Activado");
-		
-		
+	public $games = [];
+	
+	public function onEnable(){
+		@mkdir($this->getDataFolder());
+		$this->register();
 	}
 	
-	
+	public function register(){
+		$this->gameMannager = new game($this);
+		$this->eventlistener = new EventListener($this);
+	}
 }
